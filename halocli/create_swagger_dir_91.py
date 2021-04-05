f38cdcfc3ceb20e64734b055aa576dfa7cc3ebc2
halo-framework <halo-framework@gmail.com>
2021-04-05 21:18:53 +0300

    fix

--- halocli/create_swagger_dir_91.py
+++ halocli/create_swagger_dir_91.py
@@ -11,23 +11,18 @@ do_copy = False
 #[print(x[0]) for x in os.walk(directory)]
 cnt = 1
 for x in os.walk(directory):
-    #print(x)
-    if x[0].endswith('BIAN_APIs_Release9.0'):
-        continue
     dir_files = x[2]
     if len(dir_files) == 0:
         continue
-    tmp = os.path.dirname(x[0])
-    sd_name = x[0].replace(tmp,'').replace('\\','')
-    #print(sd_name)
+    sd_name = None
     f_name = None
     json_name = None
     for f in dir_files:
-        if f.endswith('zip'):
-            f_name = f.replace('-master.zip','').replace('sd-','').replace('-v2.0','-v2')
+        if f.endswith('json'):
+            f_name = f.replace('.json','')
             obj_str = '"'+str(cnt)+'": { \
-                "f_name": "'+ f_name +'", \
-                "name": "'+ sd_name.strip() +'", \
+                "f_name": "'+ f_name.strip() +'", \
+                "name": "'+ f_name.strip() +'", \
                 "service_domain": true, \
                 "swagger": true \
             },'
